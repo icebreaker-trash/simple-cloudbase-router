56c2e8ab84369c5249763bd7edbb2ca7d11d017c
SonOfMagic <qq1324318532@gmail.com>
2021-12-29 23:11:08 +0800

    v1.0.1

--- src/compose.ts
+++ src/compose.ts
@@ -1,1 +1,9 @@
-export { default as compose } from 'koa-compose'
+import koaCompose from 'koa-compose'
+import type { Middleware } from 'koa-compose'
+import type { AnyObject, IBaseContext } from './type'
+
+export function compose<T = AnyObject> (
+  middleware: Middleware<IBaseContext & T>[]
+) {
+  return koaCompose(middleware)
+}
